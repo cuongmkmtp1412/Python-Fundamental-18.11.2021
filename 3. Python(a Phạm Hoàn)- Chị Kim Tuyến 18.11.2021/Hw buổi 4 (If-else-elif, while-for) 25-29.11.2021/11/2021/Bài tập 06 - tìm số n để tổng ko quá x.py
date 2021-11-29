e74e3478de753028057beaec486fb71a48c643fd
Đoàn Ngọc Cường <92771138+cuongmkmtp1412@users.noreply.github.com>
2021-11-29 15:54:29 +0700

    alo

--- 3. Python(a Phạm Hoàn)- Chị Kim Tuyến 18.11.2021/Hw buổi 4 (If-else-elif, while-for) 25-29.11.2021/11/2021/Bài tập 06 - tìm số n để tổng ko quá x.py
+++ 3. Python(a Phạm Hoàn)- Chị Kim Tuyến 18.11.2021/Hw buổi 4 (If-else-elif, while-for) 25-29.11.2021/11/2021/Bài tập 06 - tìm số n để tổng ko quá x.py
@@ -6,7 +6,8 @@ Bài 06. Nhập vào một số tự nhiên x,
 x = int(input("Nhập số tự nhiên x = ")) # số thực cũng được 
 sum = 0
 n = 0
-while sum <= x:              # chú ý các đầu mút khi thiết lập điều kiện
+while sum <= x:           # chú ý các đầu mút khi thiết lập điều kiện
     n += 1
     sum += n
 print(f'Số nguyên dương n lớn nhất mà tổng các số tự nhiên đến n không vượt quá {x} là {n-1}')
+# Hôm ở trên lớp cứ nghĩ theo hướng làm vòng lặp for, lú cả đầu => ko ra. 
